fe96f94b815b1673e16419cf92a3139ff7aa39b6
Senthil Murugan Manikantan <smanikantan@blackducksoftware.com>
2017-07-19 13:04:01 -0400

    Incorporated James review comments
    * Added new LinkView
    * Removed the Super() from the constructor

--- src/main/java/com/blackducksoftware/integration/hub/model/view/components/LinkView.java
+++ src/main/java/com/blackducksoftware/integration/hub/model/view/components/LinkView.java
@@ -1,17 +1,16 @@
-/**
- * Hub Common Response
- *
- * Copyright (C) 2017 Black Duck Software, Inc.
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
  * http://www.blackducksoftware.com/
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
+ * regarding copyright ownership.
+ *
+ * The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
@@ -23,34 +22,24 @@
  */
 package com.blackducksoftware.integration.hub.model.view.components;
 
-import java.util.List;
-
 import com.blackducksoftware.integration.hub.model.HubComponent;
-import com.blackducksoftware.integration.hub.model.enumeration.AllowEnum;
 
-public final class MetaView extends HubComponent {
-    private final List<AllowEnum> allow;
+public final class LinkView extends HubComponent {
+    private final String rel;
 
     private final String href;
 
-    private final List<LinkView> links;
-
-    public MetaView(List<AllowEnum> allow, String href, List<LinkView> links) {
-        this.allow = allow;
+    public LinkView(String rel, String href) {
+        this.rel = rel;
         this.href = href;
-        this.links = links;
     }
 
-    public List<AllowEnum> getAllow() {
-        return allow;
+    public String getRel() {
+        return rel;
     }
 
     public String getHref() {
         return href;
-    }
-
-    public List<LinkView> getLinks() {
-        return links;
     }
 
 }
