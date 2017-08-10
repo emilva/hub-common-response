105164208eb62e36c6593cadaca936856b6106cd
mpeng <mpeng@blackducksoftware.com>
2017-08-10 09:59:54 -0400

    Forgot to save RoleAssignmentView before committing

--- src/main/java/com/blackducksoftware/integration/hub/model/view/RoleAssignmentView.java
+++ src/main/java/com/blackducksoftware/integration/hub/model/view/RoleAssignmentView.java
@@ -1,5 +1,11 @@
 package com.blackducksoftware.integration.hub.model.view;
 
 public class RoleAssignmentView {
-
+	
+	public String name;
+	
+	public String description;
+	
+	public String role;
+	
 }
