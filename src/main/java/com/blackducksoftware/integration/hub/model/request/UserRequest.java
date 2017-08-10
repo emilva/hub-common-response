351378af40838a349b2423d389a91f2ea6326771
mpeng <mpeng@blackducksoftware.com>
2017-08-10 10:16:40 -0400

    Changed visibility of fields

--- src/main/java/com/blackducksoftware/integration/hub/model/request/UserRequest.java
+++ src/main/java/com/blackducksoftware/integration/hub/model/request/UserRequest.java
@@ -1,18 +1,41 @@
+/**
+ * Hub Common Response
+ *
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.blackducksoftware.integration.hub.model.request;
 
 public class UserRequest {
 	
-	private Boolean active;
+	public Boolean active;
 	
-	private String email;
+	public String email;
 	
-	private String firstName;
+	public String firstName;
 	
-	private String lastName;
+	public String lastName;
 	
-	private String password;
+	public String password;
 	
-	private String userName;
+	public String userName;
 	
 }
 
